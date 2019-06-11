90965b9a7b341678769185659ad2c84df7b2c28a
timlathy <tim.labushev@gmail.com>
2019-06-11 13:25:54 +0300

    Updates

---
+++ precache-manifest.52b1ee8ef1cba67c825508a473c6d02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "075a6a8dfff59410a222",
+    "url": "/museum-of-optics/static/js/runtime~main.7d119e98.js"
+  },
+  {
+    "revision": "8793cb20175df1a50028",
+    "url": "/museum-of-optics/static/js/main.3d0b232a.chunk.js"
+  },
+  {
+    "revision": "bb9a874a439e5e1cb3f9",
+    "url": "/museum-of-optics/static/js/2.fcea7d9c.chunk.js"
+  },
+  {
+    "revision": "8793cb20175df1a50028",
+    "url": "/museum-of-optics/static/css/main.27e736b3.chunk.css"
+  },
+  {
+    "revision": "bb9a874a439e5e1cb3f9",
+    "url": "/museum-of-optics/static/css/2.c945ad26.chunk.css"
+  },
+  {
+    "revision": "195f511b319e0b52b9fd12b471b50d29",
+    "url": "/museum-of-optics/index.html"
+  }
+];
