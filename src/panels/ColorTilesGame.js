7c6ce43d7021e4323af7d7bcaa6389a0955aebb5
timlathy <tim.labushev@gmail.com>
2019-06-18 23:09:32 +0300

    tiles: game state

--- src/panels/ColorTilesGame.js
+++ src/panels/ColorTilesGame.js
@@ -7,24 +7,45 @@ import yellowTile from '../img/tile-button-yellow.png';
 import redTile from '../img/tile-button-red.png';
 import greenTile from '../img/tile-button-green.png';
 
-export default function ColorTilesGame(props) {
-  return (
-    <Panel id={props.id} theme="white">
-      <PanelHeader left={<PanelHeaderBack onClick={props.go} data-to="home" />}>
-        Какой твой цвет?
-      </PanelHeader>
-      <FormLayout>
+export default class ColorTilesGame extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      directions: 'Приготовился? Нажми на любой цвет:',
+      answers: 0
+    };
+  }
+
+  tileSelected = (color) => () => {
+    if (this.state.answers === 3) {
+      this.setState({ directions: 'Поздравляем, ты прошел игру)' });
+      return;
+    }
+
+    this.setState({ directions: color, answers: this.state.answers + 1 });
+  }
+
+  render() {
+    return (
+      <Panel id={this.props.id} theme="white">
+        <PanelHeader left={<PanelHeaderBack onClick={this.props.go} data-to="home" />}>
+          Какой цвет твой?
+        </PanelHeader>
         <Div>
+          <div className="tile-directions">{this.state.directions}</div>
           <div className="tile-row">
-            <button className="tile-button" style={{ backgroundImage: `url('${blueTile}')` }} />
-            <button className="tile-button" style={{ backgroundImage: `url('${redTile}')` }} />
+            <button onClick={this.tileSelected('blue')} className="tile-button" style={{ backgroundImage: `url('${blueTile}')` }} />
+            <button onClick={this.tileSelected('red')} className="tile-button" style={{ backgroundImage: `url('${redTile}')` }} />
           </div>
           <div className="tile-row">
-            <button className="tile-button" style={{ backgroundImage: `url('${yellowTile}')` }} />
-            <button className="tile-button" style={{ backgroundImage: `url('${greenTile}')` }} />
+            <button onClick={this.tileSelected('yellow')} className="tile-button" style={{ backgroundImage: `url('${yellowTile}')` }} />
+            <button onClick={this.tileSelected('green')} className="tile-button" style={{ backgroundImage: `url('${greenTile}')` }} />
+          </div>
+          <div className="tile-help">
+            Встань лицом к каталогу Аббе
           </div>
         </Div>
-      </FormLayout>
-    </Panel>
-  );
+      </Panel>
+    );
+  }
 }
