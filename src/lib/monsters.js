c171641287666bf530fd5815ccd6a7dc457164a5
timlathy <tim.labushev@gmail.com>
2019-06-22 16:42:18 +0300

    monster definitions wip

---
+++ src/lib/monsters.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import seIfmoRu from '../img/monsters/se_ifmo_ru.png';
+
+const monsters = {
+  2: {
+    name: 'Утиная типизация',
+    sprite: seIfmoRu,
+    description: 'И сколько в твоем интерпретаторе сегфолтов?',
+    hints: (
+      <p>
+        Добрый день, я Утинатор, и сегодня я хочу рассказать вам о том,
+        почему вы должны бросить все дела и заставить себя учить Форт.
+      </p>
+    )
+  }
+};
+
+export default monsters;
