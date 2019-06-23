1aabf32c589dba1bfd526c885be2d4b6dee7430b
johnny-keker <giomail.iv@gmail.com>
2019-06-23 18:19:46 +0300

    tile game logic finally done

--- src/lib/ColorTilesGame.js
+++ src/lib/ColorTilesGame.js
@@ -21,15 +21,15 @@ export function computeTurn(color, x, y, ans, routes) {
   // compute next tile
   var direction = Math.floor(Math.random() * 4); // random int [0; 3]
   while (
-    (routes[direction] == 'left' && y == 0) ||
-    (routes[direction] == 'up' && x == 0) ||
-    (routes[direction] == 'right' && y == 5) ||
-    (routes[direction] == 'down' && x == 5))
+    (direction == 0 && y == 0) ||
+    (direction == 1 && x == 0) ||
+    (direction == 2 && y == 5) ||
+    (direction == 3 && x == 5))
     direction = Math.floor(Math.random() * 4);
   var offset = 0, strDirection = routes[direction];
+  console.log(routes);
   switch (direction) {
     case 0:
-      routes = routes.slice(3).concat(routes.slice(0,3));
       offset = 1 + Math.floor(Math.random() * y);
       y = y - offset;
       break;
@@ -38,16 +38,28 @@ export function computeTurn(color, x, y, ans, routes) {
       x = x - offset;
       break;
     case 2:
-      routes = routes.slice(1).concat(routes[0]);
       offset = 1 + Math.floor(Math.random() * (5 - y));
       y = y + offset;
       break;
     case 3:
-      routes = routes.slice(2).concat(routes.slice(0,2));
       offset = 1 + Math.floor(Math.random() * (5 - x));
       x = x + offset;
       break;
   }
+  switch (routes[direction]) {
+    case 'left':
+      routes = routes.slice(1).concat(routes[0]);
+      break;
+    case 'up':
+      break;
+    case 'right':
+      routes = routes.slice(3).concat(routes.slice(0,3));
+      break;
+    case 'down':
+      routes = routes.slice(2).concat(routes.slice(0,2));
+      break;
+  }
+  console.log(direction, x, y);
   return {
     directions: `${strDirection}, ${offset}`,
     routes: routes,
