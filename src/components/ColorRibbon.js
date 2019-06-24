727a6d36c01409f2db4ac808bd94fcd4053d69c5
timlathy <tim.labushev@gmail.com>
2019-06-24 19:52:41 +0300

    fancier color ribbon

--- src/components/ColorRibbon.js
+++ src/components/ColorRibbon.js
@@ -5,8 +5,8 @@ export default function ColorRibbon(props) {
   const itemClass = props.animated ? 'color-ribbon__item color-ribbon__item--animated' : 'color-ribbon__item';
   return (
     <div className="color-ribbon">
-      {props.colors.map((c) =>
-        <div className={`${itemClass} color-ribbon__item--${c}`}></div>)}
+      {props.colors.map((c, i) =>
+        <div key={i} className={`${itemClass} color-ribbon__item--${c}`}></div>)}
     </div>
   );
 }
