00a372c310e4acb5e0c4f2ca28191ae6aaf59d49
timlathy <tim.labushev@gmail.com>
2019-06-24 18:23:12 +0300

    improve monster page layout

--- src/views/Monster.js
+++ src/views/Monster.js
@@ -3,6 +3,7 @@ import { View, Panel, PanelHeader, Div, Button } from '@vkontakte/vkui';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
 import PanelHeaderClose from '@vkontakte/vkui/dist/components/PanelHeaderClose/PanelHeaderClose';
 import '../css/Monster.css';
+import qrCodeExample from '../img/qr-code-example.svg';
 import monsters from '../lib/monsters';
 import { updateSavestateOnCompletion } from '../lib/savestate';
 
@@ -28,18 +29,18 @@ export default class Monster extends React.Component {
 
   renderIntroPanel({ name, sprite, description }) {
     return (
-      <Panel id="monster-intro" theme="white">
+      <Panel id="monster-intro" theme="white" centered={true}>
         <PanelHeader left={<PanelHeaderBack onClick={this.props.go} data-to="quest-map" />}>
           {name}
         </PanelHeader>
-        <Div style={{ textAlign: "center" }}>
+        <div className="monster-main">
           <img className="monster-sprite" alt={name} src={sprite} />
           <p>{description}</p>
-          <div className="button-row">
-            <Button size="l" level="1" onClick={this.setView('monster-action')}>В бой</Button>
-            <Button size="l" level="2" onClick={this.setView('monster-hint')}>Подсказка</Button>
-          </div>
-        </Div>
+        </div>
+        <div className="monster-buttons">
+          <Button size="l" level="1" onClick={this.setView('monster-action')}>В бой</Button>
+          <Button size="l" level="2" onClick={this.setView('monster-hint')}>Подсказка</Button>
+        </div>
       </Panel>
     );
   }
@@ -50,22 +51,24 @@ export default class Monster extends React.Component {
         <PanelHeader left={<PanelHeaderClose children="Закрыть" onClick={this.setView('monster-intro')} />}>
           {name}
         </PanelHeader>
-        <Div style={{ textAlign: "center" }}>{hint}</Div>
+        <Div>{hint}</Div>
       </Panel>
     );
   }
 
   renderActionPanel({ name, correctQr }) {
     return (
-      <Panel id="monster-action" theme="white">
+      <Panel id="monster-action" theme="white" centered={true}>
         <PanelHeader left={<PanelHeaderClose children="Закрыть" onClick={this.setView('monster-intro')} />}>
           {name}
         </PanelHeader>
-        <Div style={{ textAlign: "center" }}>
-          <div>
-            <Button size="l" level="1" onClick={this.applyQrCode(correctQr)}>Применить</Button>
-          </div>
-        </Div>
+        <p className="monster-action-tip">
+          Найди в музее нужный артефакт:
+        </p>
+        <img alt="Пример QR-кода" src={qrCodeExample} className="monster-action-qr" />
+        <div className="monster-buttons">
+          <Button size="l" level="1" onClick={this.applyQrCode(correctQr)}>Применить</Button>
+        </div>
       </Panel>
     );
   }
@@ -88,20 +91,26 @@ export default class Monster extends React.Component {
 
   renderResultPanel({ name, spriteDefeated, onVictory }) {
     const completed = <>
-      <img className="monster-sprite" alt={name} src={spriteDefeated} />
-      <p>{onVictory}</p>
-      <Button size="l" level="1" onClick={this.props.go} data-to="quest-map">Далее</Button>
+      <div className="monster-main">
+        <img className="monster-sprite" alt={name} src={spriteDefeated} />
+        <p>{onVictory}</p>
+      </div>
+      <div className="monster-buttons">
+        <Button size="l" level="1" onClick={this.props.go} data-to="quest-map">Далее</Button>
+      </div>
     </>;
     const failed = <>
-      <p>Очень жаль, но код неправильный :/</p>
-      <Button size="l" level="1" onClick={this.setView('monster-action')}>Вернуться</Button>
+      <div className="monster-main">
+        <p>К сожалению, этот артефакт никак не подействовал на монстра. Подумай еще ;)</p>
+      </div>
+      <div className="monster-buttons">
+        <Button size="l" level="1" onClick={this.setView('monster-action')}>Вернуться</Button>
+      </div>
     </>;
     return (
-      <Panel id="monster-result" theme="white">
+      <Panel id="monster-result" theme="white" centered={true}>
         <PanelHeader>{name}</PanelHeader>
-        <Div style={{ textAlign: "center" }}>
-          {this.state.completed ? completed : failed}
-        </Div>
+        {this.state.completed ? completed : failed}
       </Panel>
     );
   }
