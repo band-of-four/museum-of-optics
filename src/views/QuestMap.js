b5f1b896a4947b2ec6a4cc74adb90398738b4c5d
timlathy <tim.labushev@gmail.com>
2019-06-28 10:27:13 +0300

    quest map: help section

--- src/views/QuestMap.js
+++ src/views/QuestMap.js
@@ -1,28 +1,58 @@
-import React, { useEffect, useRef } from 'react';
-import { View, Panel, PanelHeader } from '@vkontakte/vkui';
+import React, { useState } from 'react';
+import { View, Panel, PanelHeader, FixedLayout } from '@vkontakte/vkui';
 import PanelHeaderBack from '@vkontakte/vkui/dist/components/PanelHeaderBack/PanelHeaderBack';
+import Icon28HelpOutline from '@vkontakte/icons/dist/28/help_outline';
 import '../css/QuestMap.css';
-import { nodeLocations, computePaths, isForestUnlocked, isSavestateClean } from '../lib/savestate';
+import { nodeLocations, computePaths, isForestUnlocked } from '../lib/savestate';
 
 export default function QuestMap({ id, go, savestate }) {
-  const mapBottomRef = useRef(null);
-  useEffect(() => {
-    mapBottomRef.current && setTimeout(() => mapBottomRef.current.scrollIntoView({ behavior: 'smooth' }), 100);
-  }, []);
-
+  const [view, setView] = useState('quest-map-main');
   return (
-    <View id={id} activePanel="quest-map-main">
+    <View id={id} activePanel={view}>
       <Panel id="quest-map-main" theme="white">
-        <PanelHeader left={<PanelHeaderBack onClick={go} data-to="home" />}>
-          Карта
-        </PanelHeader>
+        <PanelHeader left={<PanelHeaderBack onClick={go} data-to="home" />}>Карта</PanelHeader>
+        <FixedLayout vertical="top">
+          <div className="map-help-overflow">
+            <button className="map-help-button" onClick={() => setView('quest-map-help')}>
+              <span className="map-help-button__content"><Icon28HelpOutline /></span>
+            </button>
+          </div>
+        </FixedLayout>
         <div className="map-container">
           <svg className="map" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 68 264">
             {nodeLocations.map(renderMapNode(savestate, go))}
             {renderForest(savestate, go)}
             {renderPaths(savestate)}
           </svg>
-          {isSavestateClean(savestate) && <div ref={mapBottomRef} />}
+        </div>
+      </Panel>
+      <Panel id="quest-map-help" theme="white" centered>
+        <PanelHeader left={<PanelHeaderBack onClick={() => setView('quest-map-main')} />}>Записки странника</PanelHeader>
+        <div className="map-help-container">
+          <svg className="map" xmlns="http://www.w3.org/2000/svg" width="78" height="78" viewBox="0 0 20.6 20.6">
+            <g className="map-node--locked">
+              <path stroke-linejoin="bevel" d="M19.7 10.5a9.6 9.6 0 0 1-9.6 9.6 9.6 9.6 0 0 1-9.6-9.6A9.6 9.6 0 0 1 10.1 1a9.6 9.6 0 0 1 9.6 9.5z" />
+              <path stroke-linejoin="round" d="M6.6 9.2h2.2L5.4 13H9v3.6h2.2V13h3.5l-3.3-3.8h2.1l-3.4-4.7z" />
+            </g>
+          </svg>
+          <p>
+            Дорога к Оптическому Лесу — не секрет, но попасть в него может далеко не каждый!
+          </p>
+          <p>
+            Сначала тебе придется победить всех монстров, что охраняют его, используя оптические артефакты.
+          </p>
+          <svg className="map" xmlns="http://www.w3.org/2000/svg" width="78" height="78" viewBox="0 0 20.6 20.6">
+            <circle cx="10" cy="10" r="6" className="map-node--available" />
+          </svg>
+          <p>
+            Нажми на открытую тебе точку на карте, чтобы сразиться с монстром.
+          </p>
+          <svg className="map" xmlns="http://www.w3.org/2000/svg" width="78" height="78" viewBox="0 0 20.6 20.6">
+            <circle cx="10" cy="10" r="6" className="map-node--locked" />
+          </svg>
+          <p>
+            С каждой победой тебе будут открываться новые точки, которые недоступны сейчас.
+          </p>
         </div>
       </Panel>
     </View>
