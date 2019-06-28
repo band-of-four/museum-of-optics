764cecc640cbcadcc92c3a2d29aa79ea617f202c
timlathy <tim.labushev@gmail.com>
2019-06-28 13:36:14 +0300

    preload tile buttons, darken them on touch

--- src/components/ColorTiles.js
+++ src/components/ColorTiles.js
@@ -1,16 +1,5 @@
 import React from 'react';
 import '../css/ColorTiles.css';
-import blueTile from '../img/tiles/blue.png';
-import yellowTile from '../img/tiles/yellow.png';
-import redTile from '../img/tiles/red.png';
-import greenTile from '../img/tiles/green.png';
-
-const tileUrls = {
-  b: `url('${blueTile}')`,
-  r: `url('${redTile}')`,
-  y: `url('${yellowTile}')`,
-  g: `url('${greenTile}')`
-};
 
 // colors: an array of 4 items, each one of 'b', 'r', 'y', 'g'
 // onClick: an array of 4 callback functions, one for each of the buttons
@@ -21,7 +10,8 @@ export default function ColorTiles(props) {
   const buttonClass = (props.buttonClass || '') + ' ColorTiles__button' + (props.inline ? ' ColorTiles__button--inline' : '');
 
   const buttons = props.colors.map((color, i) =>
-    <button key={i} onClick={props.onClick[i]} className={buttonClass} style={{ backgroundImage: tileUrls[color] }}>
+    <button key={i} onClick={props.onClick[i]} className={`ColorTiles-${color} ${buttonClass}`}>
+      <span className="ColorTiles__button__fill" />
       {props.labels && props.labels[i]}
     </button>
   );
